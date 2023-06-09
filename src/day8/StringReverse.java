072ff77f3ec89859127c3f2773cf9009686d9430
rohith <rohithrao0588@gmail.com>
2023-06-09 16:10:39 -0500

    Strings

---
+++ src/day8/StringReverse.java
@@ -0,0 +1,20 @@
+package day8;
+
+public class StringReverse {
+    public static void main (String args[]) {
+        String input="Java J2EE Reverse Me";
+        String[] words=input.split(" ");
+        String revWords="";
+
+        for(int i=0;i<words.length;i++) {
+            String word = words[i];
+            String revword="";
+            for(int j=word.length()-1;j>=0;j--) {
+                revword=revword+word.charAt(j);
+            }
+            revWords=revWords+revword;
+        }
+        System.out.println(revWords);
+
+    }
+}

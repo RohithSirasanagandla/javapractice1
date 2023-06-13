6fb25f1b3067260c050bc05adc3510a5a290ce13
rohith <rohithrao0588@gmail.com>
2023-06-13 15:04:17 -0500

    done

--- src/day9/CustomException.java
+++ src/day9/CustomException.java
@@ -1,2 +1,7 @@
-package day9;public class CustomException {
+package day9;
+
+public class CustomException extends Exception{
+    public CustomException(String message) {
+        super(message);
+    }
 }
--- src/day9/StringToIntegerConverter.java
+++ src/day9/StringToIntegerConverter.java
@@ -1,2 +1,24 @@
-package day9;public class StringToIntegerConverter {
+package day9;
+
+public class StringToIntegerConverter {
+    public static int convertToInteger(String str) throws NumberFormatException {
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("Cannot convert '" + str + "' to an integer");
+        }
+    }
+    public static void main(String[] args) {
+        String[] inputs = {"23", "45.67", "test", "123f"};
+
+        for (String input : inputs) {
+            try {
+                int result = convertToInteger(input);
+                System.out.println("Input: " + input + ", Converted: " + result);
+            } catch (NumberFormatException e) {
+                System.out.println("Input: " + input + ", Exception: " + e.getMessage());
+
+            }
+        }
+    }
 }

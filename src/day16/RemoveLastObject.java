4b99303b19a3201526dcaf1a26fe46c8d604d2d1
rohith <rohithrao0588@gmail.com>
2023-06-20 15:11:16 -0500

    array

---
+++ src/day16/RemoveLastObject.java
@@ -0,0 +1,19 @@
+package day16;
+import java.util.ArrayList;
+
+public class RemoveLastObject {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("apple");
+        list.add("banana");
+        list.add("cherry");
+
+        System.out.println("ArrayList before removal: " + list);
+
+        if (!list.isEmpty()) {
+            list.remove(list.size() - 1);
+        }
+
+        System.out.println("ArrayList after removal: " + list);
+    }
+}
